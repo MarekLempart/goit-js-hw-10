fdaf46639de0dae1d94d6c817a39c517f7f2b030
MarekLempart <kama.zakupy@gmail.com>
2024-02-15 21:49:25 +0100

    Praca domowa hw10_02

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
 //live_wR84NoD0Zi8ijz9x8CliBmxrXIlf6XHm7mlzgn1u8ueopN77ta1ls607iNekAUuW
 
-const fetchBreedsBtn = document.querySelector('.btn');
+// import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
-fetchBreedsBtn.addEventListener('click', async () => {
+const fetchBreedsBtn = document.querySelector('.btn');
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.catInfo');
+const loader = document.querySelector('.loader');
+
+fetchBreedsBtn.addEventListener('click', () => {
   try {
-    const breeds = await fetchBreeds();
-    console.log(breeds);
+    loader.classList.remove('hidden');
+    fetchBreeds().then(data => renderSelect(data));
   } catch (error) {
     console.log(error);
   }
 });
 
-async function fetchBreeds() {
-  const response = await axios.get(`https://api.thecatapi.com/v1/breeds`);
-  return response.data;
+// async function fetchBreeds() {
+//   const response = await axios.get(`https://api.thecatapi.com/v1/breeds`);
+//   return response.data;
+// }
+
+function renderSelect(breeds) {
+  const markup = breeds
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>`;
+    })
+    .join('');
+  breedSelect.insertAdjacentHTML('beforeend', markup);
+  loader.classList.add('hidden');
+}
+
+breedSelect.addEventListener('change', e => {
+  fetchCatByBreed(e.target.value).then(data => renderCat(date[0]));
+});
+
+// https://api.thecatapi.com/v1/images/search
+
+function renderCat(catDate) {
+  const { url } = catDate;
+  const { description, name, temperament } = catDate.breeds[0];
+  catInfo.insertAdjacentHTML(
+    'beforeend',
+    `<div>
+      <h2>${name}</h2>
+      <img src="${name} />
+      <p>${description}</p>
+      <p><strong>Temperament:</strong> ${temperament}</p>
+    </div>`
+    // `<option value="${id}">${name}</option>`
+  );
 }
